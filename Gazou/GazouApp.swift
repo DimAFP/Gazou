5d24c74be39075889ba9b912df6d24b7848d00a7
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-27 13:32:50 +0200

    rarar

---
+++ Gazou/GazouApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazouApp.swift
+//  Gazou
+//
+//  Created by Apprenant 08 on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GazouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
